b7d9e44ca2351535e992d36c717969cc33573750
Alvaro de Ocio <losocio143@gmail.com>
2021-12-12 13:22:21 +0100

    Siguiente version del software

--- MEMsym.c
+++ MEMsym.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 #define TAM_LINEA 16
 #define NUM_FILAS 8
 
@@ -12,17 +11,58 @@ typedef struct{
 
 //Prototipos de funciones necesarias
 void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]);
-void MostrarCACHE(T_CACHE_LINE *tbl);
+void MostrarCACHE(T_CACHE_LINE tbl[NUM_FILAS]);
 
 int main(){
-	unsigned int globaltime, numfallos;
+	unsigned int globaltime, numfallos, addr, fin_fichero;
 	globaltime = 0;
 	numfallos = 0;
+	addr = 0;
+	fin_fichero = 1;
+
+	unsigned char Simul_RAM[4096];
 
 	T_CACHE_LINE datos_cache[NUM_FILAS];
 
+	FILE *p_CONTENTS_RAM;
+	FILE *p_accesos_memoria;
+
+	p_CONTENTS_RAM = fopen("CONTENTS_RAM.bin", "rb");
+	p_accesos_memoria = fopen("accesos_memoria.txt", "r");
+
+/*	Este me da problemas, no se porque
+	if(p_CONTENTS_RAM || p_accesos_memoria == NULL){
+                printf("Alguno de los ficheros que ha intentado leer no existe\n");
+                return -1;
+        }
+*/
+
+	if(p_CONTENTS_RAM  == NULL){
+		printf("p_CONTENTS_RAM\n");
+		return -1;
+	}
+
+	if(p_accesos_memoria == NULL){
+                printf("p_accesos_memoria\n");
+                return -1;
+        }
+
 	LimpiarCACHE(datos_cache);
-	MostrarCACHE(datos_cache);
+        MostrarCACHE(datos_cache);
+
+	while(fscanf(p_CONTENTS_RAM, "%c", Simul_RAM) != EOF);
+
+	do{
+		//while(fscanf(p_accesos_memoria, "%X", &addr) != '\n');
+		if(fscanf(p_accesos_memoria, "%X", &addr) != EOF) printf("%X\n", addr);
+		else fin_fichero = 0;
+
+
+		//sleep(1);
+	}while(fin_fichero);
+
+	fclose(p_CONTENTS_RAM);
+	fclose(p_accesos_memoria);
 
 	return 0;
 }
@@ -40,7 +80,7 @@ void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]){
 }
 
 //Funcion que muestra el estado actual de la cache por pantalla
-void MostrarCACHE(T_CACHE_LINE *tbl){
+void MostrarCACHE(T_CACHE_LINE tbl[NUM_FILAS]){
 	int i, j;
 	for(i = 0; i < NUM_FILAS; i++){
 		printf("ETQ:%X  DATA", tbl[i].ETQ);
